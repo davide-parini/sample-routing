a1d9a2e8a4666bdcc150e415adfb7b8aef79d831
paro- <paro-@DESKTOP-M7UTGOP.homenet.telecomitalia.it>
2018-04-05 23:07:48 +0200

    Riorganizzazione context.

--- CurrenciesAPI/CurrenciesAPI_WebApp/src/main/java/it/chebanca/sampleintegration/api/CurrenciesApiController.java
+++ CurrenciesAPI/CurrenciesAPI_WebApp/src/main/java/it/chebanca/sampleintegration/api/CurrenciesApiController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.chebanca.sampleintegration.api.mapper.CountryApiMapper;
@@ -12,6 +13,7 @@ import it.chebanca.sampleintegration.common.TechnicalException;
 import it.chebanca.sampleintegration.integration.CountriesRestService;
 
 @RestController
+@RequestMapping("/api")
 public class CurrenciesApiController {
 
 	@Autowired
